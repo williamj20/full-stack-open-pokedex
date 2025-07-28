55da0aa17ede4a06d45221706a41f216af39cecd
williamj20 <williamjiang20@gmail.com>
2025-07-28 16:36:48 -0400

    finish 11.9

--- e2e-tests/pokedex.test.spec.js
+++ e2e-tests/pokedex.test.spec.js
@@ -1,10 +1,17 @@
 const { test, describe, expect } = require('@playwright/test')
 
-
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('pokemon can be navigated to', async ({ page }) => {
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
